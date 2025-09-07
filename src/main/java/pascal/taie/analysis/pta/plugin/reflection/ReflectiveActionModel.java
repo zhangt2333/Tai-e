de451dc81eb215c10eeb6bcb5e82b95b41dc6184
Teng Zhang <zhangt2333@gmail.com>
2025-09-07 13:43:30 +0800

    Test checkstyle in PR

--- src/main/java/pascal/taie/analysis/pta/plugin/reflection/ReflectiveActionModel.java
+++ src/main/java/pascal/taie/analysis/pta/plugin/reflection/ReflectiveActionModel.java
@@ -1,25 +1,3 @@
-/*
- * Tai-e: A Static Analysis Framework for Java
- *
- * Copyright (C) 2022 Tian Tan <tiantan@nju.edu.cn>
- * Copyright (C) 2022 Yue Li <yueli@nju.edu.cn>
- *
- * This file is part of Tai-e.
- *
- * Tai-e is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
- *
- * Tai-e is distributed in the hope that it will be useful,but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
- * Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Tai-e. If not, see <https://www.gnu.org/licenses/>.
- */
-
 package pascal.taie.analysis.pta.plugin.reflection;
 
 import pascal.taie.analysis.graph.callgraph.Edge;
@@ -133,9 +111,8 @@ public class ReflectiveActionModel extends AnalysisModelPlugin {
     @InvokeHandler(signature = "<java.lang.reflect.Constructor: java.lang.Object newInstance(java.lang.Object[])>", argIndexes = {BASE})
     public void constructorNewInstance(Context context, Invoke invoke, PointsToSet constructors) {
         constructors.forEach(obj -> {
-            if (isInvalidTarget(invoke, obj)) {
+            if (isInvalidTarget(invoke, obj))
                 return;
-            }
             JMethod constructor = CSObjs.toConstructor(obj);
             if (constructor != null && !typeMatcher.isUnmatched(invoke, constructor)) {
                 ClassType type = constructor.getDeclaringClass().getType();
